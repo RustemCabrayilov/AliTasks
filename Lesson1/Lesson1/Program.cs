bb3aa40ca763f738794770f478f4ebbe84fe3006
NijatS <nijatjs@code.edu.az>
2024-03-12 15:35:49 +0400

    add:Lesson2

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,249 @@
+﻿#region For ForEach
+
+//for(int i = 0; i < 10; i+=2)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+//for(int i = 10; i > 0; i -= 2)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+
+//int[] numbers = { 1, 4, 6, 7, 8, 9 };
+
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    Console.WriteLine(numbers[i]);
+//}
+
+//foreach (int number in numbers)
+//{
+//    Console.WriteLine(number);
+//}
+
+#endregion
+
+#region While DoWhile
+
+//int i = 0;
+//int j = 0;
+
+
+//while (i<0)
+//{
+//    Console.WriteLine("Ali");
+//    i++;
+//}
+
+
+//do
+//{
+//	Console.WriteLine("Nijat");
+//	j++;
+//}
+//while (j < 0);
+
+
+//char i = 'A';
+
+//do
+//{
+
+//	Console.WriteLine(i);
+//	i++;
+
+//} while (i <= 'Z');
+
+//Console.ReadLine();
+
+
+
+#endregion
+
+#region Break Continue
+
+//Break
+//for (int i = 0; i < 10; i++)
+//{
+//	if (i == 5)
+//	{
+//		break;
+//	}
+//	Console.WriteLine(i);
+//}
+////Isliyecek
+//Console.WriteLine("Ali");
+
+//Continue
+//for (int i = 0; i < 10; i++)
+//{
+//	if (i == 5)
+//	{
+//		continue;
+//	}
+//	Console.WriteLine(i);
+//}
+
+
+//Return
+//for (int i = 0; i < 10; i++)
+//{
+//	if (i == 5)
+//	{
+//		return;
+//	}
+//	Console.WriteLine(i);
+//}
+////Islemiyecek
+//Console.WriteLine("Ali");
+
+#endregion
+
+#region Arrays ArrayList
+
+//int[] numbers = { 2, 5, 7, 8, 3, 5, 10 };
+
+//Console.WriteLine(numbers.Max(x => x));
+
+//int max = numbers[0];
+
+//for (int i = 1; i < numbers.Length; i++)
+//{
+//	if (numbers[i] > max)
+//	{
+//		max = numbers[i];	
+//	}
+//}
+//Console.WriteLine("Max Value : "+max);
+
+using System.Collections;
+
+ArrayList datas = new ArrayList();
+
+datas.Add(0);
+datas.Add(1);
+datas.Add(14);
+datas.Add(-3);
+datas.Add(11);
+datas.Add(5);
+
+//Hansini Silmek istiyirsen
+//datas.Remove(11);
+
+//Hansi indexdekin silmek istiyirsen
+//datas.RemoveAt(2);
+
+//datas.Add("Ali");
+
+//Istenilen Indexe elave eliyir
+//datas.Insert(2, "Ali");
+
+//Azdan Coxa ve ya Elifba sirasina uygun
+//datas.Sort();
+
+//datas.Reverse();
+
+//for (int i = 0; i < datas.Count; i++)
+//{
+//    Console.WriteLine(datas[i]);
+//}
+
+//List<int> numbers = new List<int>();
+//numbers.Add(0);
+//numbers.Add(1);
+//numbers.Add(14);
+//numbers.Add(-3);
+//numbers.Add(11);
+//numbers.Add(5);
+
+//Console.WriteLine(numbers.Max());
+
+#endregion
+
+#region TaskIfElse
+//float odeyeceyimebleg;
+//Console.Write("meblegi girin: ");
+//float xerc = float.Parse(Console.ReadLine());
+
+//if(xerc >= 100 && xerc <200)
+//{
+//	odeyeceyimebleg = (float)(xerc * 0.9);
+//}
+//else if(xerc >= 200 && xerc <300 )
+//{
+//	odeyeceyimebleg = (float)(xerc * 0.85);
+//}
+//else 
+//{
+//	odeyeceyimebleg = (float)(xerc * 0.8);
+//}
+
+//Console.WriteLine("Sizin xerciniz "+odeyeceyimebleg);
+#endregion
+
+List<int> numbers = new List<int>();
+numbers.Add(1);
+numbers.Add(2);
+numbers.Add(3);
+numbers.Add(1);
+numbers.Add(2);
+numbers.Add(1);
+numbers.Add(2);
+numbers.Add(3);
+numbers.Add(4);
+numbers.Add(5);
+numbers.Add(6);
+numbers.Add(-6);
+
+
+//for (int i = 0; i < numbers.Count; i++)
+//{
+//	int count = 1;
+//	for (int j = 1; j < numbers.Count; j++)
+//	{
+//		if (numbers[i] == numbers[j])
+//		{
+//			count++;
+
+//		}
+//	}
+//	Console.WriteLine("Number:" + numbers[i] + "  Count: " + count);
+//}
+
+for (int i = 0; i < numbers.Count; i++)
+{
+	for (int j = i + 1; j < numbers.Count; j++)
+	{
+		int temp;
+		if (numbers[i] > numbers[j])
+		{
+			temp = numbers[j];
+			numbers[j] = numbers[i];
+			numbers[i] = temp;
+		}
+	}
+}
+
+for (int i = 0; i < numbers.Count; i++)
+{
+	int count = 1;
+	for (int j = i + 1; j < numbers.Count; j++)
+	{
+		if (numbers[i] == numbers[j])
+		{
+			count++;
+		}
+		else
+		{
+			i = j-1 ;
+			break;
+		}
+	}
+	Console.WriteLine("Number:" + numbers[i] + "  Count:" + count);
+}
+
---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,242 @@
+﻿
+
+#region DataTypes
+//string name = "Ali";
+//name = "Nijat";
+//Console.Write("Eded girin:");
+//int a = int.Parse(Console.ReadLine());
+
+//Console.WriteLine(a);
+
+//Console.Write("Bugun derse getmisen?");
+//bool cavab =bool.Parse( Console.ReadLine());
+//Console.WriteLine(cavab);
+
+#endregion
+
+#region RiyaziIslemler
+
+//int a = 5;
+//int b = 10;
+//a += 5;  //a=a+5
+//a -= 10; //a=a-10
+
+//a *= 5; //a=a*5
+//a += 1;
+//a++;
+
+//Console.WriteLine(a+5);
+//Console.WriteLine(a);
+//Console.WriteLine(++a);
+
+
+
+//int a = 5;
+
+//Console.WriteLine(a++);
+//Console.WriteLine(++a);
+
+#endregion
+#region Sertler IFElse
+#region Task1
+/*Task 1 : Tələbə İmtahan Qiymətini Daxil Edir. Daxil Edilən İmtahan Qiyməti 0 və 100 Aralığında Olmalıdır.
+Əgər Qiymət 91 və 100 Aralığındadırsa A Çap Etsin.
+Əgər Qiymət 81 və 90 Aralığındadırsa B Çap Etsin.
+Əgər Qiymət 71 və 80 Aralığındadırsa C Çap Etsin.
+Əgər Qiymət 61 və 70 Aralığındadırsa D Çap Etsin.
+Əgər Qiymət 51 və 60 Aralığındadırsa E Çap Etsin.
+Əgər Qiymət 51 -dən Kiçikdirsə Kəsildiniz Çap Etsin.
+*/
+//Console.Write("Imtahan balinizi daxil edin:");
+
+//double bal =double.Parse(Console.ReadLine());
+
+//if(bal >100 || bal < 0)
+//{
+//    Console.WriteLine("Duzgun bal daxil edin");
+//}
+//else
+//{
+//	if (bal >= 91 && bal <= 100)
+//	{
+//		Console.WriteLine("Sizin baliniz A");
+//	}
+//	else if (bal >= 81 && bal < 91)
+//	{
+//		Console.WriteLine("Sizin baliniz B");
+//	}
+//	else if (bal >= 71 && bal < 81)
+//	{
+//		Console.WriteLine("Sizin baliniz C");
+//	}
+//	else if (bal >= 61 && bal < 71)
+//	{
+//		Console.WriteLine("Sizin baliniz D");
+//	}
+//	else if (bal >= 51 && bal < 61)
+//	{
+//		Console.WriteLine("Sizin baliniz E");
+//	}
+//	else
+//	{
+//		Console.WriteLine("Siz imtahanan kesildiniz");
+//	}
+//}
+#endregion
+#region Task2
+//Console.Write("Yasinizi daxil edin:");
+//int yas = int.Parse(Console.ReadLine());
+//if (yas <= 0)
+//{
+//    Console.WriteLine("Yasinizi duzgun daxil edin");
+//}
+//else
+//{
+//	if (yas >= 12)
+//	{
+//		Console.WriteLine("web sayfasina kayit oldunuz");
+//	}
+//	else
+//	{
+//		Console.WriteLine("yasinizdan dolayi kayit olamadiniz");
+//	}
+//}
+#endregion
+#region Task3
+//Console.Write("Alışveriş tutarını giriniz:");
+//double tutar = Convert.ToDouble(Console.ReadLine());
+
+//double endirimtutari = 0.0;
+//if (tutar >= 300)
+//{
+//	endirimtutari = 0.20;
+//}
+//else if (tutar >= 200)
+//{
+//	endirimtutari = 0.15;
+//}
+//else if (tutar >= 100)
+//{
+//	endirimtutari = 0.10;
+//}
+
+//double endirimOlunacaqFiyat = tutar * endirimtutari;
+//double odenecekTutar = tutar - endirimOlunacaqFiyat;
+
+//Console.WriteLine("Odenilecek mebleg: "+odenecekTutar + " TL");
+//Console.WriteLine("Endirim olunan mebleg: "+ endirimOlunacaqFiyat + " TL");
+
+#endregion
+#endregion
+
+#region SwitchCase
+#region Task1
+//Console.Write("Gunu ededle daxil edin:");
+//int gun = int.Parse(Console.ReadLine());
+
+//switch (gun)
+//{
+//	case 1:
+//		Console.WriteLine("Monday");
+//		break;
+//	case 2:
+//		Console.WriteLine("Tuesday");
+//		break;
+//	case 3:
+//		Console.WriteLine("Wednesday");
+//		break;
+//	case 4:
+//		Console.WriteLine("Thursday");
+//		break;
+//	case 5:
+//		Console.WriteLine("Friday");
+//		break;
+//	case 6:
+//		Console.WriteLine("Saturday");
+//		break;
+//	case 7:
+//		Console.WriteLine("Sunday");
+//		break;
+//	default:
+//        Console.WriteLine("Duzgun daxil etmediniz..Gunler(1-7)araliginda olur");
+//        break;
+//}
+
+#endregion
+#region Task2
+
+//Console.Write("Isiqforun rengini daxil edin:");
+//string reng = Console.ReadLine();
+//switch (reng.ToLower())
+//{
+//	case "yasil":
+//        Console.WriteLine("Davam ede bilersen");
+//		break;
+//	case "sari":
+//        Console.WriteLine("Hazirlasin");
+//		break;
+//	case "qirmizi":
+//        Console.WriteLine("Dayanin");
+//		break;
+//    default:
+//        Console.WriteLine("Rengi duzgun daxil edin");
+//        break;
+//}
+
+#endregion
+#endregion
+
+
+#region Donguler For
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine("Hello World!");
+//}
+
+//for (int i = 0; i < 100; i+=2)
+//{
+//    Console.WriteLine(i);
+//}
+
+//for (int i = 1; i <= 10; i++)
+//{
+//	for (int j = 1; j <= i; j++)
+//	{
+//        Console.Write(j+" ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+//for (int i = 1; i < 100; i *= 2)
+//{
+//    Console.WriteLine(i);
+//}
+
+//for (int i = 100; i >= 1; i-=2)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+//for (int i =1; i < 100; i=i*3)
+//{
+//    Console.WriteLine(i);
+//}
+
+//int[setir,sutun]
+int[,] ededler = { { 1, 2, 3 }, { 4,5,6} ,{ 7,8,9} };
+
+
+for (int i = 0; i < 3; i++)
+{
+	for (int j = 0; j < 3; j++)
+	{
+		Console.Write(ededler[i,j]+" ");
+    }
+	Console.WriteLine();
+}
+
+
+#endregion
