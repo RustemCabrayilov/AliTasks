176a2ebc31e71b57bcf349c2c98d488b6cd26078
NijatS <nijatjs@code.edu.az>
2024-05-16 15:01:45 +0400

    add:ORM

---
+++ ORM/ORM/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM.Models
+{
+	public class Student
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string Address { get; set; }
+
+		public override string ToString()
+		{
+			return "ID:" + Id + " Name:" + Name + " Surname:" + Surname;
+		}
+	}
+}
---
+++ ORM/ORM/Program.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using ORM.Context;
+using ORM.Models;
+
+namespace ORM
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			//Student student = new Student()
+			//{
+			//	Name = "Ali2",
+			//	Surname = "Agayev",
+			//	Address = "28 May"
+			//};
+
+			//SMDbContext context = new SMDbContext();
+			using (SMDbContext context = new SMDbContext())
+			{
+
+				#region Add
+				//EntityEntry entityState = context.Students.Add(student);
+
+				//Console.WriteLine(entityState.State);
+				//context.SaveChanges();
+				//Console.WriteLine(entityState.State);
+
+				#endregion
+				#region Query
+
+				//var students = from s in context.Students
+				//						 select new
+				//						 {
+				//							 Id = s.Id,
+				//							 Name = s.Name,
+				//							 Surname = s.Surname,
+				//							 Address = s.Address,
+				//						 };
+
+				//foreach(var student in students)
+				//{
+				//                Console.WriteLine(student.Name);
+				//            }
+
+
+				//var students = context.Students.Where(s=> s.Name.ToLower().Contains("ali"));
+
+				//foreach (var student in students)
+				//{
+				//                Console.WriteLine(student);
+				//            }
+
+				#endregion
+
+				#region UpdateRemove
+
+				//var student = context.Students.Where(s => s.Id ==6).FirstOrDefault();
+
+
+				//context.Students.Remove(student);
+				//context.SaveChanges();
+
+
+				var student = context.Students.Where(s=>s.Id ==7).FirstOrDefault();
+				if (student != null)
+				{
+                    Console.WriteLine(context.Entry(student).State);
+					student.Name = "ALi";
+					Console.WriteLine(context.Entry(student).State);
+
+
+					context.SaveChanges();
+					Console.WriteLine(context.Entry(student).State);
+
+				}
+
+
+				#endregion
+
+			};
+
+
+		}
+	}
+}
