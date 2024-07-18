e4e8397d663d4d1cc5d38f1ac84fb18852335f49
NijatS <nijatjs@code.edu.az>
2024-07-18 22:43:15 +0400

    add:MVCProject

---
+++ AvtosRestoran/AvtosRestoran/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AvtosRestoran.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Her bir actionun ozune uygun view si var
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AvtosRestoran/AvtosRestoran/Controllers/ServiceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AvtosRestoran.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Booking()
+        {
+            return View();
+        }
+    }
+}
